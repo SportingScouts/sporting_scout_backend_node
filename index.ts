e3379ab836da3b7aea8d4eb1821ec3b4b49a8fc4
Kareem Elmetwally <kareem.hossam1996@gmai.com>
2023-07-30 12:42:41 +0300

    inital commit, added request logging middleware using morgan

--- index.ts
+++ index.ts
@@ -2,12 +2,14 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import router from "./src/routes";
+import morgan from "morgan";
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
